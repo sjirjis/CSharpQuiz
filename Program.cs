810e6e20eb0592a50a727660b39d71787149f448
unknown <sjirjis@gmail.com>
2017-11-28 18:47:17 -0800

    initial push

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpQuiz
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string dupString = "mississippi river";
+            Console.WriteLine(removeDups(dupString));
+            Console.Read();
+        }
+
+        static string removeDups(string str)
+        {
+            str.ToCharArray();
+            List<char> charsToRemove = new List<char>();
+            string noDupStr = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (noDupStr.Contains(str[i])){
+                    charsToRemove.Add(str[i]);
+                }
+                else
+                {
+                    noDupStr = noDupStr + str[i];
+                }
+            }
+
+            foreach (var c in charsToRemove)
+            {
+                noDupStr = noDupStr.Replace(c, ' ');
+            }
+            return noDupStr;
+        }
+    }
+}
